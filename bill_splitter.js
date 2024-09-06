df01be118c478aef8a24e911b75951533e5cd650
Quirianyailet Perez <qperez1@usf.edu>
2024-09-06 17:19:17 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,7 +1,14 @@
 // Task1: Calculate the Tip
-if {50 <= bill <= 300 ?
-tip = '15%' : '20%'
-}
+if 50 <= bill <= 300 ?
+tip = '0.15' : '.20'
 console.log(tip(true)); // Output " Bill is between 50 and 300"
 console.log(tip(false)); // Output "Bill is not between 50 and 300"
+// Task2: Output Details
+var displayBillingDetails(billamount,tiprate,totalamount) {
+    const tipamount = billamount * tiprate;
+    const totalamount = tiprate + billamount;
+console.log(`The bill was $275, the tip was $41.25, and the total value is $316.25`); 
+}
+//Example 
+displayBillingDetails(275,0.15,316.25); //Output billamount: $275, tiprate: 15%, Total $316.25
 
