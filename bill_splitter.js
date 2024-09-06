2fc97982f6ff68eabc188d5fe8ecf3da7d45a6e2
Quirianyailet Perez <qperez1@usf.edu>
2024-09-06 17:53:59 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,14 +1,17 @@
 // Task1: Calculate the Tip
-if 50 <= bill <= 300 ?
-tip = '0.15' : '.20'
-console.log(tip(true)); // Output " Bill is between 50 and 300"
-console.log(tip(false)); // Output "Bill is not between 50 and 300"
+let billamount = 200 // Example
+const tip =(billamount >= 50 && billamount <= 300) ? (billamount*0.15): (billamount*0.20);
+console.log(tip); // Output tip amount
 // Task2: Output Details
 var displayBillingDetails(billamount,tiprate,totalamount) {
     const tipamount = billamount * tiprate;
     const totalamount = tiprate + billamount;
-console.log(`The bill was $275, the tip was $41.25, and the total value is $316.25`); 
+console.log(`The bill was $200, the tip was $30, and the total value is $230`); 
 }
 //Example 
-displayBillingDetails(275,0.15,316.25); //Output billamount: $275, tiprate: 15%, Total $316.25
-
+displayBillingDetails(200,0.15,230); //Output billamount: $200, tiprate: 15%, Total $300
+// Task3: Create a Function
+function calculateTip(billamount,tiprate) {
+    return billamount * tiprate;
+}
+console.log($$calculateTip (100,0.15)); // Testing Function with a bill amount of 100 and tax rate 15%
