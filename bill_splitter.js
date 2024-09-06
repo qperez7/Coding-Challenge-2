6b9303148e4bb0408d004d84dff212a5ff19acd5
Quirianyailet Perez <qperez1@usf.edu>
2024-09-06 19:02:43 -0400

    Implement and populate arrays for bills, tips, and totals

--- bill_splitter.js
+++ bill_splitter.js
@@ -3,15 +3,22 @@ let billamount = 200 // Example
 const tip =(billamount >= 50 && billamount <= 300) ? (billamount*0.15): (billamount*0.20);
 console.log(tip); // Output tip amount
 // Task2: Output Details
-var displayBillingDetails(billamount,tiprate,totalamount) {
+function displayBillingDetails (billamount,tiprate) {
     const tipamount = billamount * tiprate;
     const totalamount = tiprate + billamount;
 console.log(`The bill was $200, the tip was $30, and the total value is $230`); 
 }
 //Example 
-displayBillingDetails(200,0.15,230); //Output billamount: $200, tiprate: 15%, Total $300
+displayBillingDetails(200,0.15,230); //Output billamount: $200, tiprate: 15%, Total $230
 // Task3: Create a Function
 function calculateTip(billamount,tiprate) {
     return billamount * tiprate;
 }
-console.log($$calculateTip (100,0.15)); // Testing Function with a bill amount of 100 and tax rate 15%
+console.log($$calculateTip (100,0.15)); // Testing Function with a bill amount of 100 and tip rate 15%
+// Task4: Utilize Arrays
+let bills =[275,40,430,125,555,44]; //  Bills Array
+const tips = bills.map(bills => calculateTip(bills)); //Tips Array using map
+const totals = bills.map(bills => tips ); // Gives totals of each bill and tip sum
+console.log("billamounts:", bills);
+console.log("tipgiven",tips);
+console.log("eachtotal",totals);
