74ec9344e7529c0b06844c2070018965de3c3d2c
Quirianyailet Perez <qperez1@usf.edu>
2024-09-06 20:10:56 -0400

    Test Data

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,20 +1,20 @@
 // Task1: Calculate the Tip
 let billamount = 200 // Example
 const tip =(billamount >= 50 && billamount <= 300) ? (billamount*0.15): (billamount*0.20);
-console.log(tip); // Output tip amount
+console.log(tip); // Output tip amount 30
 // Task2: Output Details
 function displayBillingDetails (billamount,tiprate) {
     const tipamount = billamount * tiprate;
     const totalamount = tiprate + billamount;
-console.log(`The bill was $200, the tip was $30, and the total value is $230`); 
+console.log(`The bill was $200, the tip was $30, and the total value is $230`); // Display Message
 }
 //Example 
 displayBillingDetails(200,0.15,230); //Output billamount: $200, tiprate: 15%, Total $230
 // Task3: Create a Function
 function calculateTip(billamount,tiprate) {
     return billamount * tiprate;
-}
 console.log($$calculateTip (100,0.15)); // Testing Function with a bill amount of 100 and tip rate 15%
+}
 // Task4: Utilize Arrays
 let bills =[275,40,430,125,555,44]; //  Bills Array
 const tips = bills.map(bills => calculateTip(bills)); //Tips Array using map
